0eac58a9ad33eae936b351f466e697e95d5d685a
Kulakov Oleg <oakulako@mts.ru>
2023-01-15 12:28:41 +0300

    optimize algorithm

--- sorts_visualize/sort_algorithms/default.py
+++ sorts_visualize/sort_algorithms/default.py
@@ -6,23 +6,30 @@ class Default(BaseSortAlgorithm):
         super().__init__(data)
         self.name = 'default'
         self.is_direct = True
-        self.position = 0
+        self.start_position = 0 if self.is_direct else len(self.data) - 1
+        self.end_position = len(self.data) - 1 if self.is_direct else 0
+        self.current_position = self.start_position
 
     def sort(self):
         if self.is_direct:
-            if self.data[self.position] > self.data[self.position + 1]:
-                self.data[self.position], self.data[self.position + 1] = self.data[self.position + 1], self.data[
-                    self.position]
-            self.position += 1
-            if self.position == len(self.data) - 1:
+            if self.current_position < self.end_position:
+                if self.data[self.current_position] > self.data[self.current_position + 1]:
+                    self.data[self.current_position], self.data[self.current_position + 1] = self.data[self.current_position + 1], self.data[self.current_position]
+                res = [self.current_position, self.current_position + 1]
+                self.current_position += 1
+            else:
+                self.end_position -= 1
                 self.is_direct = False
-            return [self.position, self.position + 1]
+                res = [self.current_position]
         else:
-            if self.data[self.position] < self.data[self.position - 1]:
-                self.data[self.position], self.data[self.position - 1] = self.data[self.position - 1], self.data[
-                    self.position]
-            self.position -= 1
-            if self.position == 0:
+            if self.current_position > self.start_position:
+                if self.data[self.current_position] < self.data[self.current_position - 1]:
+                    self.data[self.current_position], self.data[self.current_position - 1] = self.data[self.current_position - 1], self.data[self.current_position]
+                res = [self.current_position, self.current_position - 1]
+                self.current_position -= 1
+            else:
+                self.start_position += 1
                 self.is_direct = True
-            return [self.position, self.position - 1]
+                res = [self.current_position]
+        return res
 
